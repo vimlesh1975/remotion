82a4b878dbda1bd136d99816cbb888a71238ea80
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-12-07 19:30:57 +0530

    remotion

---
+++ src/app/remotion/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Composition} from 'remotion';
+import {MyComposition} from './MyComposition';
+ 
+export const RemotionRoot= () => {
+  return (
+    <>
+      <Composition
+        id="Empty"
+        component={MyComposition}
+        durationInFrames={1000}
+        fps={25}
+        width={1920}
+        height={1080}
+      />
+    </>
+  );
+};
